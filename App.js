be95dac067fee2aeef55b939e31c3f68390b47f9
Mattskywalker <mateusejesus@hotmail.com.br>
2021-04-07 14:43:13 -0300

    adicionado: modal e animatables

--- App.js
+++ App.js
@@ -1,21 +1,165 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, SafeAreaView,TouchableOpacity, FlatList, Modal, TextInput} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import TaskList from './src/components/TaskList';
+import * as Animatable from 'react-native-animatable'
+
+const AnimatableButton = Animatable.createAnimatableComponent(TouchableOpacity)
 
 export default function App() {
+  const [task,setTask] = useState([
+    {key: 1,task: 'Comprar pão'},
+    {key: 2,task: 'Farmar no clash'},
+    {key: 3,task: 'Ligar pra Lua'},
+    {key: 4,task: 'Aprender React'},
+  ])
+
+  const [open, setOpen] = useState(false)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+
+      <StatusBar backgroundColor='#171d31' barStyle="ligth-content"/>
+      <View styles={styles.content}>
+        <Text style={styles.title}>Minhas Tarefas</Text>
+      </View>
+
+      <FlatList
+      marginHorizontal={10}
+      showsHorizontalScrollIndicator={false}
+      data={task}
+      keyExtractor={(item) => String(item.key)}
+      renderItem={({item}) => <TaskList data={item}/>}
+
+      />
+
+      <Modal 
+      animationType="slide"
+      useNativeDriver
+      transparent={false}
+      visible={open}
+      >
+        <SafeAreaView style={styles.modal}>
+          <StatusBar backgroundColor='#171d31'/>
+          <View style={styles.modalLeader}>
+            <TouchableOpacity onPress={() => setOpen(false)}>
+              <Ionicons style={marginleft= 5, marginRight = 5} name="arrow-back" size={40} color='#FFF'/>
+            </TouchableOpacity>
+
+            <Text style={styles.modalTitle}></Text>
+          </View>
+
+          <View style={styles.modalBoddy} animation="fadeInUp">
+            <TextInput
+            placeholder="O que precisa fazer hoje ?"
+            placeholderTextColor='grey'
+            multiline={true}
+            
+            style={styles.input}
+            />
+
+            <TouchableOpacity style={styles.addButton}>
+              <Text style={styles.addButtonText}>Salvar</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+      </Modal>
+
+      <AnimatableButton 
+      style={styles.fab}
+      useNativeDriver
+      animation="bounceInUp"
+      duration={1500}
+      onPress={() => setOpen(true)}
+      >
+        <Ionicons name="ios-add" size={35} color='#FFF'/>
+      </AnimatableButton>
+      
+      
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#171D31',
+    
+  },
+  title:{
+    marginTop: 38,
+    fontSize:30,
+    textAlign: 'center',
+    paddingBottom:30,
+    color:'#FFF'
+  },
+  fab:{
+
+    position:'absolute',
+    width:60,
+    height:60,
+    backgroundColor: '#0094FF',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 30,
+    right: 25,
+    bottom: 25,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset:{
+      width: 1,
+      height: 3,
+    }
   },
+
+  modal:{
+    flex: 1,
+    backgroundColor: '#171d31',
+  },
+  modalLeader:{ 
+    marginLeft: 15,
+    marginTop: 11,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  modalTitle:{
+    marginLeft:15,
+    fontSize: 20,
+    color: '#FFF',
+  },
+  modalBoddy:{
+    marginTop: 15,
+    
+
+  },
+  input:{
+    fontSize: 20,
+    marginHorizontal:15,
+    marginTop: 30,
+    backgroundColor:'#FFF',
+    padding: 30,
+    height: 200,
+    textAlignVertical: 'top',
+    color: '#FFF',
+    borderRadius: 30,
+    
+  },
+  addButton:{
+    backgroundColor: '#0094FF',
+    alignItems: 'center',
+    borderRadius: 20,
+    margin: 20,
+    padding:20,
+    
+    textAlign:'center',
+  },
+  addButtonText:{
+    fontSize: 20,
+  }
+
+  
+
+
 });
---
+++ src/components/TaskList/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {View, Text,StyleSheet} from 'react-native'
+import {Ionicons} from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import * as Animatable from 'react-native-animatable'
+
+
+
+
+export default function TaskList({data}){
+    return(
+        
+        <Animatable.View 
+        style={styles.container}
+        animation="bounceIn"
+        useNativeDriver
+        >
+            <TouchableOpacity>
+                <Ionicons name='md-checkmark-circle' size={30} color='#121212'/>
+            </TouchableOpacity>
+            <View>
+                <Text style={styles.task}>{data.task}</Text>
+            </View>
+        </Animatable.View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        margin: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#3399ff',
+        borderRadius: 30,
+        padding: 7,
+        elevation: 1.5,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset:{
+            width: 1,
+            height: 3,
+        }
+        
+
+    },
+    task:{
+        color: '#FFF',
+        fontSize: 20,
+        paddingRight: 20,
+        paddingLeft: 8,
+
+    }
+})
