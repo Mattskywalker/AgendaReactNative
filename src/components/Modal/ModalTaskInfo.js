88470e5e08c024b41d90f1ba22ce1398c6e8bc3e
Mattskywalker <mateusejesus@hotmail.com.br>
2021-04-20 14:20:57 -0300

    backholder adicionado na modal de informações

--- src/components/Modal/ModalTaskInfo.js
+++ src/components/Modal/ModalTaskInfo.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{useState} from 'react';
-import {Text, View, SafeAreaView,TouchableOpacity, Modal, TextInput} from 'react-native';
+import React,{useEffect, useState} from 'react';
+import {Text, View, SafeAreaView,TouchableOpacity, Modal, TextInput, BackHandler, Alert} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 
 import dayjs from 'dayjs'    
@@ -12,10 +12,31 @@ const AnimatableButton = Animatable.createAnimatableComponent(TouchableOpacity)
 import {styleModalInfo} from '../../styles/StyleModalInfo'
 
 export default function ModalTaskInfo({infoVisible,setInfoVisivle}){
+
+    const backAction = () => {
+        Alert.alert("Espere!", "Tem certeza que quer sair?", [
+          {
+            text: "Cancelar",
+            onPress: () => null,
+            style: "cancel"
+          },
+          { text: "Sim", onPress: () => BackHandler.exitApp() }
+        ]);
+        return true;
+      };
+      
+      useEffect(()=>{
+        BackHandler.addEventListener('hardwareBackPress',backAction)
+    
+        return() => BackHandler.removeEventListener("hardwareBackPress",()=> backAction);
+      },[])
+
+    
     return(
         <Modal
         animationType={'slide'}
         useNativeDriver
+        onRequestClose={() => setInfoVisivle(false)}
         
         visible={infoVisible}>
             <SafeAreaView style={styleModalInfo.modal}>
